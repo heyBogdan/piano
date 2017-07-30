380baa89f1ffb8246a413b4257b4cd2132834b6f
Bokhonov Bogdan <bokhonovb@gmail.com>
2017-07-30 23:15:11 +0300

    Add active key, add branch gh-pages

--- frontend/components/Key.jsx
+++ frontend/components/Key.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import {connect} from "react-redux";
 import {setActiveKey} from "../actions/index.jsx"
 
 class Key extends React.Component{
+    constructor(props){
+        super(props);
+        this.keyClickHandler = this.keyClickHandler.bind(this);
+    }
     keyClickHandler(e){
+        const {store} = this.context;
         e.target.firstElementChild.load();
         e.target.firstElementChild.play();
+        store.dispatch(setActiveKey(e.target.getAttribute('data-note')));
     }
     render(){
         return (
-                <div className={"keyboard__key " + ((this.props.note.indexOf('1') == -1) ? "" : "keyboard__key--sharp")}
+                <div className={
+                        "keyboard__key " + ((this.props.note.indexOf('1') == -1) ? "" : "keyboard__key--sharp ") +
+                         ((this.props.activeKey === this.props.note) ? "keyboard__key--active" : "")
+                     }
                      onClick={this.keyClickHandler} 
                      data-note={this.props.note}>  
                     <audio id={"note-" + this.props.note}>
@@ -23,5 +33,12 @@ Key.contextTypes = {
     store: React.PropTypes.object
 };
 
+function mapStateToProps (state){
+    return{
+        activeKey:state.activeKey
+    }
+}
+
+Key = connect(mapStateToProps)(Key);
 
 export default Key
