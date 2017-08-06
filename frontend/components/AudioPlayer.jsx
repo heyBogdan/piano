fb1aafccc9acf6e625a052f1a39a573eedcafa1a
Bokhonov Bogdan <bokhonovb@gmail.com>
2017-08-06 15:12:29 +0300

    Add webm video and disable user-select

--- frontend/components/AudioPlayer.jsx
+++ frontend/components/AudioPlayer.jsx
@@ -6,6 +6,7 @@ class AudioPlayer extends React.Component{
         return (
                 <video className="modal-window__player" width="90%" autoPlay loop>
                     <source src="videos/tutorial.mp4" type="video/mp4" />
+                    <source src="videos/tutorial.webm" type="video/webm" />
                     Your browser does not support the video tag.
                 </video>
         )     
