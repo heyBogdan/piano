d251643cd2a52f8812c38b0b3cd8c2beb0064841
Bokhonov Bogdan <bokhonovb@gmail.com>
2017-08-06 15:19:51 +0300

    Add social networks links

--- frontend/components/AudioPlayer.jsx
+++ frontend/components/AudioPlayer.jsx
@@ -5,8 +5,8 @@ class AudioPlayer extends React.Component{
     render(){
         return (
                 <video className="modal-window__player" width="90%" autoPlay loop>
-                    <source src="videos/tutorial.mp4" type="video/mp4" />
                     <source src="videos/tutorial.webm" type="video/webm" />
+                    <source src="videos/tutorial.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
         )     
--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -7,11 +7,11 @@ class Footer extends React.Component{
                 <p className="footer__caption">Developed by Bokhonov Bogdan</p>
                 <p className="footer__contacts">bokhonovb@gmail.com | +7 (977) 880-58-77</p>
                 <div className="social-networks">
-                    <a className="social-networks__button social-networks__button--gh" href="">
+                    <a className="social-networks__button social-networks__button--gh" href="https://github.com/heyBogdan" target="_blank">
                     </a>
-                    <a className="social-networks__button social-networks__button--fb" href="">
+                    <a className="social-networks__button social-networks__button--fb" href="https://www.facebook.com/profile.php?id=100005185458628" target="_blank">
                     </a>
-                    <a className="social-networks__button social-networks__button--tw" href="">
+                    <a className="social-networks__button social-networks__button--tw" href="https://twitter.com/hey_bogdan" target="_blank">
                     </a>
                 </div>  
             </div>
